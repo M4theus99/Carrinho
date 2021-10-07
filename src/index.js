0738477ab611739483faf1b6eed34e385cabe146
M4theus99 <matheus314000@gmaio.com>
2021-10-07 11:41:17 -0300

    okkk

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default function Pai() {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const [qtd, setQtd] = useState(0);
+
+  function contar() {
+    setQtd{qtd + 1};
+  }
+
+ 
+return(
+  <div className="Container">
+    <h1>Contador</h1>
+
+     <div> [qtd] </div>
+      <button onClick={contar}> Contar </button>
+
+  </div>
+)
+
+}
